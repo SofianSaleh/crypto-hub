0efa7144f96c166db0d057b18c7094930884bee9
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-20 14:48:25 +0200

    :fire: Slow connection

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Switch, Link, Route } from 'react-router-dom';
 import { Layout, Typography, space } from 'antd';
 
+import { Navbar } from './components';
+
 function app() {
   return (
     <div className='app'>
-      <div className='navbar'></div>
+      <div className='navbar'>
+        <Navbar />
+      </div>
       <div className='main'></div>
       <div className='footer'></div>
     </div>
