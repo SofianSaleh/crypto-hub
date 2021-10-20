a232f983a8a3973b8356cdbad541b5b6280a58fe
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-20 19:59:21 +0200

    :fire: Finished for today implemented a search, Cryptocurrencies Page, Done one half of the home Page tommorow will finish the project and deploy it

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -11,10 +11,9 @@ const { Title} = Typography
 
 const Homepage = () => {
 
-    const { data , isFetching} = useGetCryptosQuery()
+    const { data , isFetching} = useGetCryptosQuery(10)
     const globalStats = data?.data?.stats
 
-    console.log(data)
     if(isFetching) return 'Loading...'
 
     return (
@@ -39,12 +38,12 @@ const Homepage = () => {
                 <Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} />
             </Col>
         </Row>
-        <div classNamew="home-heading-container">
+        <div className="home-heading-container">
             <Title level={2 } className='home-title'>Top 10 Cryptocurrencies in the world</Title>
             <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
         </div>
         <Cryptocurrencies simplified />
-        <div classNamew="home-heading-container">
+        <div className="home-heading-container">
             <Title level={2 } className='home-title'>Latest Crypto News</Title>
             <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
         </div>
