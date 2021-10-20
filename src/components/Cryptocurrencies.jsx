21207d8c6ae6b5637061a4cab31e40812b61ffc9
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-20 19:27:08 +0200

    :fire: Fetched from the Api

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,15 +1,15 @@
 import React, {useState} from 'react'
-import { Link } from "react-rouer-dom"
+import { Link } from "react-router-dom"
 import millify from 'millify'
 
 import { Card, Row, Col, Input } from "antd"
 
-import {useGetCrytposQuery } from '../services/cryptoApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
-    const {data:cryptoList, isfetching} = useGetCrytposQuery()
-    const [cryptos,setCryptos] = useState(cryptoList?.data?.coins)
-        console.log( cryptos)
+    const {data:cryptosList, isfetching} = useGetCryptosQuery()
+    const [cryptos,setCryptos] = useState(cryptosList?.data?.coins)
+        console.log(cryptos)
     
     return (
         <div>
