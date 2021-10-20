d918705831c5d5a83ec16190f2ebab2e344f928d
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-20 12:43:02 +0200

    :fire: Doing the barebone

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "ract-router-dom";
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='nav-container'>
+            <div className='logo-container'>
+                <Avatar />
+                <Typography.Title level={2} className='logo'>
+                    <Link></Link>    
+                </Typography.Title>
+            </div>
         </div>
     )
 }
