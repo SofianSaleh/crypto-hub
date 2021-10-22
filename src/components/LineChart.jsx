e4408d170f6c26c122a2cc3f2ba4e41842b40308
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-22 10:17:34 +0200

    :smile: Finished my first ever chart using ChartJS

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,6 +6,43 @@ const { Title } = Typography;
 
 
 const LineChart = ({coinHistory, currentPrice , coinName}) => {
+    const coinPrice = []
+    const coinTimestamp = []
+
+    for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+        coinPrice.push(coinHistory.data.history[i].price)
+        coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString())
+        
+    }
+
+
+    const data = {
+        labels: coinTimestamp,
+        datasets:[
+            {
+                label:'Price in USD',
+                data : coinPrice,
+                fill: false,
+                backgroundColor: "#0071bd",
+                backgroundColor: "#0071bd", 
+            }
+        ]
+    }
+
+
+    const options = {
+        scales:{
+            yAxes:[
+                {
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }
+            ]
+        }
+    }
+
+
     return (
         <>
         <Row className='chart-header'>
@@ -15,6 +52,7 @@ const LineChart = ({coinHistory, currentPrice , coinName}) => {
                 <Title level={5} className='current-price'>current {coinName} Price: ${currentPrice}</Title> 
             </Col>
         </Row>
+        <Line data={data} options={options} />
         </>
     )
 }
