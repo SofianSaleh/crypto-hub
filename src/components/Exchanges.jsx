cd5fd10a1697b484f9dcd3baab6e4874bbb05485
SofianSaleh <sofiane.saleh11@gmail.com>
2021-10-22 11:06:32 +0200

    :smile: finished the project

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
+import { Col, Row, Collapse, Typography, Avatar } from 'antd'
+import HTMLReactParser from 'html-react-parser'
+import millify from 'millify'
 
 import { useGetExchangesQuery } from '../services/cryptoApi'
-import Loader from 'Loader'
+import Loader from './Loader'
+
+const { Panel } = Collapse
+const { Text } = Typography
 
 const Exchanges = () => {
 const {data, isFetching} = useGetExchangesQuery()
+const exchangesList = data?.data?.exchanges
 
-console.log(data)
 
 if(isFetching) return <Loader />
 
   return (
-        <div>
-          <h1>Exchanges Page</h1>  
-        </div>
+        <>
+        <Row>
+          <Col span={6}>Exchanges</Col>
+          <Col span={6}>24h Trade Volume</Col>
+          <Col span={6}>Markets</Col>
+          <Col span={6}>Change</Col>
+        </Row>
+        <Row>
+          {exchangesList?.map((exchange,i) => (
+            <Col span={24} key={i}>
+              <Collapse>
+                <Panel
+                  key={exchange.id}
+                  header={(
+                    <Row key={exchange.id}>
+                      <Col span={6}>
+                        <Text><strong>{exchange.rank}</strong></Text>
+                        <Avatar className='exchange-image' src={exchange.iconUrl} />
+                        <Text><strong>{exchange.name}</strong></Text>
+                      </Col>
+                      <Col span={6}>{millify(exchange.volume)}</Col>
+                      <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                      <Col span={6}>{millify(exchange.marketShare)}</Col>
+                    </Row>
+                  )}
+                >
+                  {HTMLReactParser(exchange.description)}
+                </Panel>
+              </Collapse>
+            </Col>
+          ))}
+        </Row>
+        </>
     )
 }
 
